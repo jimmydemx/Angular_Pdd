1f37170448d70f414b6bbc8dc3898a32014c1bf8
jimmydemx <j_demx@yahoo.com>
2021-08-01 14:04:58 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
 
+interface Tabs{
+  item:string,
+  link: string
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+  /* data */
   title = 'pdd';
+
+  SelectedItem=-1;
+
+  tabs:Tabs[]=[{
+    item:"Men",
+    link:""
+  },{
+    item:"Woman",
+    link:""
+  },{
+    item:"Mobile",
+    link:""
+  },{
+    item:"Clothes",
+    link:""
+  },{
+    item:"Children",
+    link:""
+  },{
+    item:"Baby",
+    link:""
+  },{
+    item:"Accesories",
+    link:""
+  },{
+    item:"Books",
+    link:""
+  },{
+    item:"Perfume",
+    link:""
+  },{
+    item:"Tickets",
+    link:""
+  },{
+    item:"Coupons",
+    link:""
+  },
+  ]
+
+  /* 
+     functions
+  */
+  onClickItem(index:number){
+    this.SelectedItem=index
+
+  }
+
+
 }
